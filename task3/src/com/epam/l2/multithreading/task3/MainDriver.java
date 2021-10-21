6d2861534818a85d8750586ae4eb42f34b3d79f9
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-10-22 00:31:05 +0500

    adds multiple producer and consumer running in parallel case

--- task3/src/com/epam/l2/multithreading/task3/MainDriver.java
+++ task3/src/com/epam/l2/multithreading/task3/MainDriver.java
@@ -8,19 +8,35 @@ public class MainDriver {
     public static void main(String[] args) {
         Topic topic1 = new Topic();
         Topic topic2 = new Topic();
+        Topic topic3 = new Topic();
+        Topic topic4 = new Topic();
 
-        List<Topic> topics = new ArrayList<>(Arrays.asList(topic1, topic2));
 
-        Producer producer = new Producer(topics);
-        Consumer consumer = new Consumer(topics);
+        List<Topic> topics1 = new ArrayList<>(Arrays.asList(topic1, topic2));
+        List<Topic> topics2 = new ArrayList<>(Arrays.asList(topic3,topic4));
 
-        Thread producerThread = new Thread(producer);
-        producerThread.setName("PRODUCER");
+        Producer producer1 = new Producer(topics1);
+        Consumer consumer1 = new Consumer(topics1);
+
+        Thread producerThread = new Thread(producer1);
+        producerThread.setName("PRODUCER1");
         producerThread.start();
 
-        Thread consumerThread = new Thread(consumer);
-        consumerThread.setName("CONSUMER");
+        Thread consumerThread = new Thread(consumer1);
+        consumerThread.setName("CONSUMER1");
         consumerThread.start();
+
+
+        Producer producer2 = new Producer(topics2);
+        Consumer consumer2 = new Consumer(topics2);
+
+        Thread producerThread2 = new Thread(producer2);
+        producerThread2.setName("PRODUCER2");
+        producerThread2.start();
+
+        Thread consumerThread2 = new Thread(consumer2);
+        consumerThread2.setName("CONSUMER2");
+        consumerThread2.start();
 
     }
 }
