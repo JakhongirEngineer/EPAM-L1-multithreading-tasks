a3213a1ad4071e5cdfe84f5f59d81143f997abb8
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-10-22 21:43:12 +0500

    uses ConcurrentHashMap

---
+++ task1/src/com/epam/l1/multithreading/task1/hasMapVersion/DriverForConcurrentHashMapVersion.java
@@ -0,0 +1,20 @@
+package com.epam.l1.multithreading.task1.hasMapVersion;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DriverForConcurrentHashMapVersion {
+    public static void main(String[] args) {
+        Map<Integer,Integer> map = new ConcurrentHashMap<>();
+        WriterToTheMap writerToTheMap = new WriterToTheMap(map);
+        ValueSummerOfTheMap valueSummerOfTheMap = new ValueSummerOfTheMap(map);
+
+        Thread writerThread = new Thread(writerToTheMap);
+        writerThread.setName("WRITER_THREAD");
+        writerThread.start();
+
+        Thread summerThread = new Thread(valueSummerOfTheMap);
+        summerThread.setName("SUMMER_THREAD");
+        summerThread.start();
+    }
+}
