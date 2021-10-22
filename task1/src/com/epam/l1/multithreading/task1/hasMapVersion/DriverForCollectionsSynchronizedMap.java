bb0674f7ba5edf6fbf5de2f7891ce4beb2b5e3ec
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-10-22 21:46:50 +0500

    uses collections.synchronizedMap with ConcurrentHashMap

---
+++ task1/src/com/epam/l1/multithreading/task1/hasMapVersion/DriverForCollectionsSynchronizedMap.java
@@ -0,0 +1,22 @@
+package com.epam.l1.multithreading.task1.hasMapVersion;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DriverForCollectionsSynchronizedMap {
+    public static void main(String[] args) {
+        Map<Integer,Integer> map = Collections.synchronizedMap(new ConcurrentHashMap<>());
+        WriterToTheMap writerToTheMap = new WriterToTheMap(map);
+        ValueSummerOfTheMap valueSummerOfTheMap = new ValueSummerOfTheMap(map);
+
+        Thread writerThread = new Thread(writerToTheMap);
+        writerThread.setName("WRITER_THREAD");
+        writerThread.start();
+
+        Thread summerThread = new Thread(valueSummerOfTheMap);
+        summerThread.setName("SUMMER_THREAD");
+        summerThread.start();
+    }
+}
