67b59796fab33b6f3a3914772d8bc4a3c3e64cc4
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-10-23 15:28:46 +0500

    creates user and exchangeRates.txt files

---
+++ task5/src/com/epam/l1/multithreading/task5/CurrencyExchangeApplication.java
@@ -0,0 +1,7 @@
+package com.epam.l1.multithreading.task5;
+
+public class CurrencyExchangeApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
