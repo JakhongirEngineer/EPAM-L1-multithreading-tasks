3e98f29a611cc991eb2ed39145185849d8dcc423
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-10-24 12:33:34 +0500

    modifies exchangeRates.txt file

--- task5/src/com/epam/l1/multithreading/task5/repositories/ExchangeRateRepositoryImpl.java
+++ task5/src/com/epam/l1/multithreading/task5/repositories/ExchangeRateRepositoryImpl.java
@@ -2,14 +2,19 @@ package com.epam.l1.multithreading.task5.repositories;
 
 import com.epam.l1.multithreading.task5.entities.Currency;
 import com.epam.l1.multithreading.task5.entities.ExchangeRate;
+import com.epam.l1.multithreading.task5.utils.FileHandlerUtils;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 public class ExchangeRateRepositoryImpl implements ExchangeRateRepository{
     @Override
-    public Optional<ExchangeRate> findByFromAndTo(Currency from, Currency to) {
-        return Optional.empty();
+    public Optional<ExchangeRate> findByFromAndTo(Currency from, Currency to, String fileName) throws IOException {
+        List<String> exchangeRates = FileHandlerUtils.readFileLineByLine(fileName);
+
+
+
     }
 
     @Override
